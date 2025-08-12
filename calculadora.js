634f2d830ce452991380986eac996577e1d54243
IgorHerculano <play03855@gmail.com>
2025-08-12 10:27:08 -0300

    Atividade 1 resolvida e teste de servidor.

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+numero1 = 0;
+numero2 = 0;
+operacao = "/";
+
+if (operacao == "+"){resultado = numero1 + numero2;}
+else if (operacao == "-"){resultado = numero1 - numero2;}
+else if (operacao == "*"){resultado = numero1 * numero2;}
+else if (operacao == "/"){resultado = numero1 / numero2;}
+
+console.log("seu resultado é", resultado);
